3336483a212e5eb9917f261e649db9d6602c169e
richbharti154 <116936700+richbharti154@users.noreply.github.com>
2023-03-02 11:26:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ToDoList
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bpocki)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+import TodoList from './components/TodoList';
+
+export default function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
